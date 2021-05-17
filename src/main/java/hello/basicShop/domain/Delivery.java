72026c9d3229edd63adf2c2129189457691f0866
Eco-MIn <guldor10.b@gmail.com>
2021-05-17 16:04:17 +0900

    InHeritace and BaseEntity

--- src/main/java/hello/basicShop/domain/Delivery.java
+++ src/main/java/hello/basicShop/domain/Delivery.java
@@ -1,14 +1,18 @@
 package hello.basicShop.domain;
 
+import hello.basicShop.domain.base.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity @Getter @Setter
-public class Delivery {
+@Entity
+@Getter
+@Setter
+public class Delivery{
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String city;
